a00567fd31141494d5d0e0eb904ff04ef8e0dc57
Wain <wain-pc@mail.ru>
2016-01-30 21:13:29 +0300

    Fixed wrong controller location for Zone 2

--- src/app/zone2/zone2.controller.js
+++ src/app/zone2/zone2.controller.js
@@ -3,40 +3,17 @@
 
     angular
         .module('yamahaUi')
-        .controller('Zone1Controller', function ($scope, pubSub, yamahaBackend) {
+        .controller('Zone2Controller', function ($scope, pubSub, yamahaBackend) {
             var vm = this,
                 watcherSet = false;
 
             pubSub.subscribe('backend:change', $scope, function (event, data) {
                 var zoneId;
                 //this will fill in the zones
-                vm.zone = data.zones[0];
+                vm.zone = data.zones[1];
                 zoneId = vm.zone.id;
                 if(!watcherSet) {
                     watcherSet = true;
-                    //set watcher (if not previously set)
-                    vm.zone.sleepTimers = [
-                        {
-                            name: 'Off',
-                            icon: 'timer'
-                        },
-                        {
-                            name: '30"',
-                            icon: 'timer'
-                        },
-                        {
-                            name: '60"',
-                            icon: 'timer'
-                        },
-                        {
-                            name: '90"',
-                            icon: 'timer'
-                        },
-                        {
-                            name: '120"',
-                            icon: 'timer'
-                        }
-                    ];
                     if(zoneId) {
                         pubSub.addEvent({
                             name: 'frontend:change:'+zoneId,
@@ -46,18 +23,10 @@
                         });
                     }
                 }
-                $scope.$watchCollection('zone1.zone', function (newValue, oldValue) {
+                $scope.$watchCollection('zone2.zone', function (newValue, oldValue) {
                     //send request with some property back to the service here
                     pubSub.notify('frontend:change:'+newValue['id'], oldValue);
                 });
             }, true);
-
-            vm.openMenu = function ($mdOpenMenu, ev) {
-                $mdOpenMenu(ev);
-            };
-
-            vm.setScene = function (sceneNumber) {
-                yamahaBackend.setMainZoneScene(sceneNumber+1);
-            }
         });
 })();
